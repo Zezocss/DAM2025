9dfee77cf4287620f1f5be156b982aa93bc32ec1
Tiago Ferreira <101589769+JustOverDrive@users.noreply.github.com>
2025-01-11 18:37:01 +0000

    Update SupabaseClient.kt

--- app/src/main/java/data/network/SupabaseClient.kt
+++ app/src/main/java/data/network/SupabaseClient.kt
@@ -4,10 +4,10 @@ import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.gotrue.GoTrue
 
 object SupabaseClient {
-    val client = createSupabaseClient{
-        supabaseUrl = https://cdnhesvkytedtxvimved.supabase.co
+    val client = createSupabaseClient(
+        supabaseUrl = https://cdnhesvkytedtxvimved.supabase.co ,
         supabasekey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNkbmhlc3ZreXRlZHR4dmltdmVkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2MTk0NTIsImV4cCI6MjA1MjE5NTQ1Mn0.OSG-5szrCP8tYuMZN97qOjPlGlMpRSik-5Nf860dySo
-        ) { SupabaseClientBuilder
+        ) { this: SupabaseClientBuilder
             install(GoTrue)
         }
     }

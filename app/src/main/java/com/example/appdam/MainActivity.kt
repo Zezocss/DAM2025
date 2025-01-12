464536aa844052a5e4fd56890bd56987d2cce773
Tiago Ferreira <101589769+JustOverDrive@users.noreply.github.com>
2025-01-12 03:15:01 +0000

    ok

--- app/src/main/java/com/example/appdam/MainActivity.kt
+++ app/src/main/java/com/example/appdam/MainActivity.kt
@@ -4,6 +4,22 @@ package com.example.appdam
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.appdam.adapter.MainCategoryAdapter
@@ -14,12 +30,21 @@ import com.example.appdam.entidades.CategoriaItens
 import com.example.appdam.entidades.Receitas
 import com.example.appdam.interfaces.GetDataService
 import com.example.appdam.retrofitclient.RetrofitClient
+import data.model.UserState
 import kotlinx.coroutines.launch
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import utils.LoadingComponent
+
 
 class MainActivity : BaseActivity(), EasyPermissions.RationaleCallbacks, EasyPermissions.PermissionCallbacks {
     private var READ_STORAGE_PERM = 123
@@ -139,6 +164,113 @@ class MainActivity : BaseActivity(), EasyPermissions.RationaleCallbacks, EasyPer
             }
         }
     }
+
+
+    /*
+    * supabase
+    *
+    * S
+    *  U
+    *   P
+    *    A
+    *    B
+    *   A
+    *  S
+    * E
+    *
+    * supabase
+     */
+    @Composable
+    fun MainScreen(
+        viewModel: SupabaseAuthViewModel = viewModel(),
+    ) {
+        val context = LocalContext.current
+        val userState by viewModel.userState
+
+        var userEmail by remember { mutableStateOf("") }
+        var userPassword by remember { mutableStateOf("") }
+
+        var currentUserState by remember { mutableStateOf("") }
+
+        LaunchedEffect(Unit) {
+            viewModel.isUserLoggedIn(context)
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            TextField(
+                value = userEmail,
+                placeholder = {
+                    Text(text = "Enter email")
+                },
+                onValueChange = {
+                    userEmail = it
+                }
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            TextField(
+                value = userPassword,
+                placeholder = {
+                    Text(text = "Enter password")
+                },
+                onValueChange = {
+                    userPassword = it
+                }
+            )
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(onClick = {
+                viewModel.signUp(
+                    context,
+                    userEmail,
+                    userPassword,
+                )
+            }) {
+                Text(text = "Sign Up")
+            }
+            Button(onClick = {
+                viewModel.login(
+                    context,
+                    userEmail,
+                    userPassword,
+                )
+            }) {
+                Text(text = "Login")
+            }
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
+                onClick = {
+                    viewModel.logout()
+                }
+            ) {
+                Text(text = "Log Out")
+            }
+
+            when(userState) {
+                is UserState.Loading -> {
+                    LoadingComponent()
+                }
+                is UserState.Success -> {
+                    val message = (userState as UserState.Success).message
+                    currentUserState = message
+                }
+                is UserState.Error -> {
+                    val message = (userState as UserState.Error).message
+                    currentUserState = message
+                }
+
+            }
+
+            if(currentUserState.isNotEmpty()) {
+                Text(text = currentUserState)
+            }
+
+        }
+    }
+
+
 }
 
 
--- app/src/main/java/com/example/appdam/RegisterActivity.java
+++ app/src/main/java/com/example/appdam/RegisterActivity.java
@@ -3,4 +3,7 @@ package com.example.appdam;
 import android.app.Activity;
 
 public class RegisterActivity extends Activity {
+
+
+
 }
